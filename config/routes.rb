6ac114c7890f115dd188ab92d4fec0ec845f3e99
coolphon <coolphon@o2.pl>
2011-06-29 16:37:06 +0200

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,3 +60,4 @@ SampleApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
