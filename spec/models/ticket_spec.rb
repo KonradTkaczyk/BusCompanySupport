7379737b9b717d5113e25b7dfbcb6bc6a4a36c4a
coolphon <coolphon@o2.pl>
2011-10-17 17:01:33 +0200

    addedd test for relationship user-ticket, addedd logged and not logged partials for home page

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
 
 describe Ticket do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = Factory(:user)
+    @attr = { :nameOfTrip => "City A -> City B", :dateOfTrip => Time.now.to_date, :timeOfTrip => Time.now.to_time }
+  end
+
+  it "should create a new instance give valid attributes" do
+    @user.tickets.create!(@attr)
+  end
+
+  describe "user associations" do
+
+    before(:each) do
+      @ticket = @user.tickets.create(@attr)
+    end
+
+    it "should have a user attribute" do
+      @ticket.should respond_to(:user)
+    end
+
+    it "should have the right associated user" do
+      @ticket.user_id.should == @user.id
+      @ticket.user.should == @user
+    end
+  end
 end
 
 # == Schema Information
