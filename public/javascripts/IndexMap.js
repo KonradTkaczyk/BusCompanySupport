8902be282d514f2db8fe2c3563cd3fa68d56f7c5
coolphon <coolphon@o2.pl>
2013-01-26 17:37:08 +0100

    Added shortest path showing on the open street map

--- public/javascripts/IndexMap.js
+++ public/javascripts/IndexMap.js
@@ -23,7 +23,7 @@ function init()
 	map.addControl(new OpenLayers.Control.DrawFeature(lineLayer, OpenLayers.Handler.Path));
 }
 
-function jumpTo()
+function mapMarkers()
 {
   markers.clearMarkers();
   var selectBox = document.getElementById('ticket_cityFrom');
@@ -35,7 +35,13 @@ function jumpTo()
   locationPlace = cities[selectBox.value]
   var location2 = marker(locationPlace, "images/marker2.png")
 
-	lineLayer.removeAllFeatures();
+  lineLayer.removeAllFeatures();
+	line(location,location2);
+}
+
+function line(location,location2)
+{
+
 	var points = new Array
 	(
    new OpenLayers.Geometry.Point(location.lon, location.lat),
@@ -53,9 +59,38 @@ function jumpTo()
 	var lineFeature = new OpenLayers.Feature.Vector(line, null, style);
 	lineLayer.addFeatures([lineFeature]);
 }
+
+function lines(locations)
+{
+	for (var i=0;i+1<locations.length;i++)
+	{
+		line(locations[i],locations[i+1]);
+	}
+}
+
+function ShortestPath()
+{
+	markers.clearMarkers();
+  lineLayer.removeAllFeatures();
+
+	var arr = [];
+	$("#FoundTickets tr").each(function(){
+		  arr.push($(this).find("td:first").text()); //put elements into array
+	});
+	arr.push($("#FoundTickets tr").eq(-1).find("td:eq(1)").text());//last row second column
+	arr.shift();// list of cities which will be visited during travel
+	if (arr.length == 0) return;
+	var locations = new Array;
+	for (var i=0;i<arr.length;i++)
+	{
+		locations.push(marker(cities[arr[i]],"images/marker.png"));//coordination points to draw lines
+	}
+	lines(locations);
+	console.log(locations);
+}
 function marker(locationPlace, iconAdress)
 {
-var parse = locationPlace.split(',');
+	var parse = locationPlace.split(',');
   var location = new OpenLayers.LonLat(parse[0],parse[1]).transform( fromProjection, toProjection);
   var size = new OpenLayers.Size(21,25);
   var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
