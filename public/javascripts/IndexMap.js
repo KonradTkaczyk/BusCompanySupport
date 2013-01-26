d11e80ed182530e3171aee0c05a890abc29b7ce2
coolphon <coolphon@o2.pl>
2013-01-26 10:37:23 +0100

    Refactoring of JS

--- public/javascripts/IndexMap.js
+++ public/javascripts/IndexMap.js
@@ -1,11 +1,19 @@
 var map, baseLayer, markers, lineLayer;
+  var cities = new Object();
+  cities['Radom'] = "21.15, 51.40";
+  cities['Gdansk'] = "18.633333,54.366667";
+  cities['Olsztyn'] = "20.55,53.783333";
+  cities['Szczecin'] = "14.542222, 53.438056";
+  cities['Ciechanow'] = "20.608056,52.876111";
+  cities['Warsaw'] = "21.008333, 52.232222";
+  var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+  var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+
 function init()
 {
   map = new OpenLayers.Map('map');
   baseLayer = new OpenLayers.Layer.OSM();
   map.addLayer(baseLayer);
-  var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-  var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
   var position = new OpenLayers.LonLat(19.00,52.00).transform( fromProjection, toProjection)
   map.setCenter(position,6);
   markers = new OpenLayers.Layer.Markers("Cities");
@@ -17,32 +25,15 @@ function init()
 
 function jumpTo()
 {
-  var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-  var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-  var cities = new Object();
-  cities['Radom'] = "21.15, 51.40";
-  cities['Gdansk'] = "18.633333,54.366667";
-  cities['Olsztyn'] = "20.55,53.783333";
-  cities['Szczecin'] = "14.542222, 53.438056";
-  cities['Ciechanow'] = "20.608056,52.876111";
-  cities['Warsaw'] = "21.008333, 52.232222";
   markers.clearMarkers();
   var selectBox = document.getElementById('ticket_cityFrom');
-  if (selectBox.value == "NA") return;
+  if (selectBox.value == "") return;
   var locationPlace = cities[selectBox.value]
-  var parse = locationPlace.split(',');
-  var location = new OpenLayers.LonLat(parse[0],parse[1]).transform( fromProjection, toProjection);
-  var size = new OpenLayers.Size(21,25);
-  var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-  var icon = new OpenLayers.Icon('images/marker.png',size,offset);
-  markers.addMarker(new OpenLayers.Marker(location,icon.clone()));
+  var location = marker(locationPlace, "images/marker.png");//function to place a marker on map with specific icon.
   selectBox = document.getElementById('ticket_cityTo');
-  if (selectBox.value == "NA") return;
+  if (selectBox.value == "") return;
   locationPlace = cities[selectBox.value]
-  parse = locationPlace.split(',');
-  var location2 = new OpenLayers.LonLat(parse[0],parse[1]).transform( fromProjection, toProjection);
-  icon = new OpenLayers.Icon('images/marker2.png',size,offset);
-  markers.addMarker(new OpenLayers.Marker(location2,icon.clone()));
+  var location2 = marker(locationPlace, "images/marker2.png")
 
 	lineLayer.removeAllFeatures();
 	var points = new Array
@@ -61,5 +52,15 @@ function jumpTo()
 
 	var lineFeature = new OpenLayers.Feature.Vector(line, null, style);
 	lineLayer.addFeatures([lineFeature]);
+}
+function marker(locationPlace, iconAdress)
+{
+var parse = locationPlace.split(',');
+  var location = new OpenLayers.LonLat(parse[0],parse[1]).transform( fromProjection, toProjection);
+  var size = new OpenLayers.Size(21,25);
+  var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+  var icon = new OpenLayers.Icon(iconAdress,size,offset);
+  markers.addMarker(new OpenLayers.Marker(location,icon.clone()));
+  return location
 }
 
